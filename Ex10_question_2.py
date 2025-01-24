6de60892c4d2baefc44b894e791defe93b9a4bba
Titi Ejembi <Titilope.Ejembi@gmail.com>
2025-01-24 21:20:53 +0000

    CREATE: Added exercise 10 question 2

---
+++ Ex10_question_2.py
@@ -0,0 +1,39 @@
+#prompt user to enter pin
+#supplied_pin = input("Enter your PIN: ")
+
+#Define the correct pin
+#Create a loop where the user must input the correct pin to stop the loop
+#Loop and re-run code if the incorrect pin is inputted
+import sys
+
+pin = "1234"
+
+pin != "1234":
+pin = input("Enter your PIN: ")
+        print("incorrect pin")
+else:
+        print("correct pin")
+
+
+
+
+
+
+
+# correct_pin = 1234
+#
+# supplied_pin = input("Enter your PIN: ")
+# while supplied_pin != correct_pin:
+#     print("Invalid PIN")
+#
+# if supplied_pin == correct_pin:
+#     print("Correct PIN")
+
+
+
+
+# user = pin
+# pin = (1, 2, 3)
+# supplied_pin = True
+# if user == pin and supplied_pin:
+#  print("Bank Account")
